2a93c2bc13c82c8b62e7e67685af7cc590d33e46
Adrian VC <velascocuriel_adrian@hotmail.com>
2025-10-29 12:09:51 -0600

    se suben cambios de nuevo estandar

--- src/modules/products/interfaces/products.interface.ts
+++ src/modules/products/interfaces/products.interface.ts
@@ -1,3 +1,6 @@
+import type { CreateProductsDto } from '../dto/create-products.dto';
+import type { UpdateProductsDto } from '../dto/update-products.dto';
+
 export interface IProducts {
   id: string;
   name: string;
@@ -9,7 +12,7 @@ export interface IProducts {
 export interface IProductsService {
   findAll(): Promise<IProducts[]>;
   findOne(id: string): Promise<IProducts>;
-  create(createProductsDto: any): Promise<IProducts>;
-  update(id: string, updateProductsDto: any): Promise<IProducts>;
+  create(createProductsDto: CreateProductsDto): Promise<IProducts>;
+  update(id: string, updateProductsDto: UpdateProductsDto): Promise<IProducts>;
   remove(id: string): Promise<void>;
 }
